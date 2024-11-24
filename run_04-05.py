c5cdd9bbd42973515c3b10d0fb773338b0fa3967
Zi-Yan Yuwen <ziyan.yuwen.uk@gmail.com>
2024-11-24 18:52:59 +0200

    rewrite dCl as summation of Term 5 and Term 6

--- run_04-05.py
+++ run_04-05.py
@@ -7,30 +7,38 @@ from joblib import Parallel, delayed
 import time
 
 Nz = 10
-zlist = tc.linspace(0.4, 0.5, Nz)
+zlist = tc.linspace(0.4, 0.5, 2)
 l_list = tc.linspace(50, 200, 10)
-pz_list = tc.hstack([tc.tensor([1e-8, 1e-6, 1e-4, 1e-3]), 10**tc.linspace(-2, -0.4, 9)])
-l1_list = tc.hstack([ 10**tc.linspace(-4, 0, 5)[:-1], 10**tc.linspace(0, 1, 6)[:-1], tc.linspace(10, 300, 59), (10**tc.linspace(np.log10(300), np.log10(600), 10))[1:] ])
+pz_list = 10**tc.hstack([tc.linspace(-8,-4, 5)[:-1], tc.linspace(-4, -3, 3)[:-1], tc.linspace(-3, -1, 15)[:-1], tc.linspace(-1, 0, 5)])
+lmax_list = tc.hstack([tc.linspace(800, 1000, 21)[:-1], 10**tc.linspace(3, 3.5, 5)])
+l1_list = tc.hstack([ 10**tc.linspace(-4, 0, 5)[:-1], 10**tc.linspace(0, 1, 6)[:-1], tc.linspace(10, 300, 59), (10**tc.linspace(np.log10(300), np.log10(800), 13))[1:] ])
 
 params = []
 for zindex in range(Nz):
     for l in l_list:
-        for pz in pz_list:
+        for pz_index in len(pz_list):
             for l1 in l1_list:
-                params.append([zindex, l, pz, l1])
+                params.append([zindex, l, pz_list[pz_index], lmax_list[pz_index], l1])
 
 dCl_obj = dCl.Cl_kSZ2_HI2(zlist)
 
 def compute_11_14(params):
-    zindex, l, pz, l1 = params
-    res11_nobeam_i, res11_beam_i = dCl_obj.dCl_lm_Term11(zindex, l, l1, pz, N_theta=210, dim=3, beam='both')
-    res14_nobeam_i, res14_beam_i =  dCl_obj.dCl_lp_Term14(zindex, l, l1, pz, N_theta=210, dim=3, beam='both')
-    return res11_beam_i, res14_beam_i, res11_nobeam_i, res14_nobeam_i
+    zindex, l, pz, lmax, l1 = params
+    res5_beam_i = dCl_obj.dCl_lm_Term5(zindex, l, l1, pz, l_max=lmax)
+    res6_beam_i =  dCl_obj.dCl_lp_Term6(zindex, l, l1, pz, l_max=lmax)
+    return res5_beam_i, res6_beam_i
 
 
 if __name__ == '__main__':
     
-    N_JOBS = 8
-    processed_list = Parallel(n_jobs=N_JOBS, prefer='threads')(delayed(compute_11_14)(i) for i in params)
-    res = np.array(processed_list).reshape([len(zlist), len(l_list), len(pz_list), len(l1_list), 4])
+    # N_JOBS = 4
+    # processed_list = Parallel(n_jobs=N_JOBS, prefer='threads')(delayed(compute_11_14)(i) for i in params)
+
+    processed_list = []
+    for i in params:
+        res5, res6 = compute_11_14(i)
+        print(i, '   ', end='\r')
+        processed_list.append([res5, res6])
+
+    res = np.array(processed_list).reshape([len(zlist), len(l_list), len(pz_list), len(l1_list), 2])
     np.save('res.npy', res)
