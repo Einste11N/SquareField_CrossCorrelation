4f35c4c3ff0f370ec7a5d714f7626f3900706868
Ziyan@SU <28881257@sun.ac.za>
2024-11-25 22:10:12 +0200

    modified:   run_04-05.py

--- run_04-05.py
+++ run_04-05.py
@@ -11,18 +11,18 @@ zlist = tc.linspace(0.4, 0.5, 2)
 l_list = tc.linspace(50, 200, 10)
 pz_list = 10**tc.hstack([tc.linspace(-8,-4, 5)[:-1], tc.linspace(-4, -3, 3)[:-1], tc.linspace(-3, -1, 15)[:-1], tc.linspace(-1, 0, 5)])
 lmax_list = tc.hstack([tc.linspace(800, 1000, 21)[:-1], 10**tc.linspace(3, 3.5, 5)])
-l1_list = tc.hstack([ 10**tc.linspace(-4, 0, 5)[:-1], 10**tc.linspace(0, 1, 6)[:-1], tc.linspace(10, 300, 59), (10**tc.linspace(np.log10(300), np.log10(800), 13))[1:] ])
+l1_list = tc.hstack([ 10**tc.linspace(-4, 0, 5)[:-1], 10**tc.linspace(0, 1, 6)[:-1], tc.linspace(10, 300, 59), (10**tc.linspace(np.log10(300), np.log10(600), 13))[1:] ])
 
 params = []
 for zindex in range(Nz):
     for l in l_list:
-        for pz_index in len(pz_list):
+        for pz_index in range(len(pz_list)):
             for l1 in l1_list:
                 params.append([zindex, l, pz_list[pz_index], lmax_list[pz_index], l1])
 
 dCl_obj = dCl.Cl_kSZ2_HI2(zlist)
 
-def compute_11_14(params):
+def compute(params):
     zindex, l, pz, lmax, l1 = params
     res5_beam_i = dCl_obj.dCl_lm_Term5(zindex, l, l1, pz, l_max=lmax)
     res6_beam_i =  dCl_obj.dCl_lp_Term6(zindex, l, l1, pz, l_max=lmax)
@@ -36,7 +36,7 @@ if __name__ == '__main__':
 
     processed_list = []
     for i in params:
-        res5, res6 = compute_11_14(i)
+        res5, res6 = compute(i)
         print(i, '   ', end='\r')
         processed_list.append([res5, res6])
 
